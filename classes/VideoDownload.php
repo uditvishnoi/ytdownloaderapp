e25f03dd0b4f89d7e94893680a49417ec77120d6
uditvishnoi <uditvishnoi2@gmail.com>
2018-09-16 15:06:10 +0530

    Update VideoDownload.php

--- classes/VideoDownload.php
+++ classes/VideoDownload.php
@@ -140,7 +140,6 @@ class VideoDownload
     private function getCustomProp($url, $format = null, $prop = 'dump-json', $password = null)
     {
         $arguments = [
-            '--dump-json',
             $url,
             '-o'
         ];
