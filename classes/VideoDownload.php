9527937d382fc82bb767337a5a6f653fa688c895
uditvishnoi <uditvishnoi2@gmail.com>
2018-09-22 13:43:23 +0530

    Update VideoDownload.php

--- classes/VideoDownload.php
+++ classes/VideoDownload.php
@@ -140,16 +140,16 @@ class VideoDownload
     private function getCustomProp($url, $format = null, $prop = 'dump-json', $password = null)
     {
       $arguments = [
-          '--get-thumbnail',
+          '--'.$prop,
           $url,
       ];
-        if (isset($format)) {
-            $arguments[] = '-f 22/17/18';
-        }
-        if (isset($password)) {
-            $arguments[] = '--video-password';
-            $arguments[] = $password;
-        }
+      if (isset($format)) {
+          $arguments[] = '-f '.$format;
+      }
+      if (isset($password)) {
+          $arguments[] = '--video-password';
+          $arguments[] = $password;
+      }
         $process = $this->getProcess($arguments);
         //This is needed by the openload extractor because it runs PhantomJS
         $process->setEnv(['PATH'=>$this->config->phantomjsDir]);
